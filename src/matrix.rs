6e43a2bdba2a2df3ae468b88cd5309729180d424
Oussama BOUHLEL <oussama_bouhlel@outlook.fr>
2025-01-19 15:29:41 +0100

    Add ops Add rust

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,3 +1,6 @@
+use std::ops::Add;
+
+#[derive(Debug, Clone)]
 pub struct Matrix<K> {
     pub elements: Vec<Vec<K>>,
 }
--- src/vector.rs
+++ src/vector.rs
@@ -1,3 +1,5 @@
+use std::ops::Add;
+
 pub struct Vector<K> {
     pub elements: Vec<K>,
 }
