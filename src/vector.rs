88ea3e4e65b89597cfb713f1a63638d10c9261ce
obouhlel <oussama_bouhlel@outlook.fr>
2025-01-20 10:57:14 +0100

    Add method add sub scl for vector need to be test

--- src/vector.rs
+++ src/vector.rs
@@ -1,6 +1,29 @@
-use std::ops::Add;
+use std::ops::{Add, Mul, Sub};
 
 #[derive(Debug, Clone)]
 pub struct Vector<K> {
     pub elements: Vec<K>,
+}
+
+impl<K> Vector<K>
+where 
+    K: Add<Output = K> + Copy, K: Sub<Output = K> + Copy, K: Mul<Output = K> + Copy
+{
+    pub fn add(&mut self, v: Vector<K>) {
+        if self.elements.len() != v.elements.len() {
+            panic!("The vector need to be on the same plan");
+        }
+
+        self.elements = self.elements.iter().zip(v.elements.iter()).map(|(&a, &b)| a + b).collect();
+    }
+    pub fn sub(&mut self, v: Vector<K>) {
+        if self.elements.len() != v.elements.len() {
+            panic!("The vector need to be on the same plan");
+        }
+
+        self.elements = self.elements.iter().zip(v.elements.iter()).map(|(&a, &b)| a - b).collect();
+    }
+    pub fn scl(&mut self, a: K) {
+        self.elements = self.elements.iter().map(|&n| n * a).collect();
+    }
 }
